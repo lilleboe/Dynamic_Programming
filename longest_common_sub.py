7f50e2efd3af990c6999046cfc1eb324c1db739e
Scott Lilleboe <scott.lilleboe@gmail.com>
2019-01-12 14:02:25 -0700

    longest common sub* partially coded

---
+++ longest_common_sub.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def lcs(s1, s2):
+    print(s1, s2)
+    table = np.zeros((len(s1)+1, len(s2)+1))
+    print(table.shape)
+
+    for i in range(0, len(s1)):
+        for j in range(0, len(s2)):
+            if s1[i] == s2[j]:
+                table[i+1, j+1] = 1 + table[i, j]
+            else:
+                table[i+1, j+1] = table[i+1, j]
+    print(table)
+    return table.max()
+
+
+if __name__ == '__main__':
+    s1 = ['a', 'b', 'd', 'b', 'a', 'b', 'f', 'g', 'd']
+    s2 = ['b', 'e', 't', 'f', 'd', 'b', 'f', 'a', 'f', 'r']
+    print(lcs(s1, s2))
