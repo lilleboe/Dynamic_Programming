7d5a47dab1de4dcbfe78a5d3ee031f2ec850bba9
Scott Lilleboe <scott.lilleboe@gmail.com>
2019-01-13 17:23:14 -0700

    longest path added

--- longest_common_sub.py
+++ longest_common_sub.py
@@ -2,9 +2,7 @@ import numpy as np
 
 
 def lcs(s1, s2):
-    print(s1, s2)
     table = np.zeros((len(s1)+1, len(s2)+1))
-    print(table.shape)
 
     for i in range(0, len(s1)):
         for j in range(0, len(s2)):
@@ -13,10 +11,12 @@ def lcs(s1, s2):
             else:
                 table[i+1, j+1] = table[i+1, j]
     print(table)
-    return table.max()
+    return int(table.max())
 
 
 if __name__ == '__main__':
     s1 = ['a', 'b', 'd', 'b', 'a', 'b', 'f', 'g', 'd']
     s2 = ['b', 'e', 't', 'f', 'd', 'b', 'f', 'a', 'f', 'r']
+    print(s1)
+    print(s2)
     print(lcs(s1, s2))
---
+++ longest_path.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+def lp(classes):
+    print(len(classes))
+    n = len(classes)
+    dist = np.copy(classes)
+    print(dist)
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dist[i][j] = max(dist[i][j], dist[i][k] + dist[k][j])
+
+    print(dist)
+    return int(dist.max()+1)
+
+
+if __name__ == '__main__':
+    i = -np.inf
+
+    s = [[0, 1, i, i, i, i],
+         [i, 0, 1, i, i, i],
+         [i, i, 0, i, i, i],
+         [i, i, i, 0, 1, i],
+         [i, i, i, i, 0, i],
+         [i, i, i, i, i, 0]]
+
+    s = [[0, 1, i, 1, i, i, i, i],
+         [i, 0, 1, i, i, i, i, i],
+         [i, i, 0, 1, i, i, i, i],
+         [i, i, i, 0, i, i, i, 1],
+         [i, i, i, 1, 0, i, i, i],
+         [i, i, i, i, i, 0, 1, i],
+         [i, i, i, i, i, i, 0, i],
+         [i, i, i, i, i, i, i, 0]]
+
+    print(s)
+    print(lp(s))
+
+
