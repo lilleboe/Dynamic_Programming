d978881681ca2018de8e81c024d3036ff0f6777f
Scott Lilleboe <scott.lilleboe@gmail.com>
2019-01-14 19:05:54 -0700

    test value changes

--- money_bag_2.py
+++ money_bag_2.py
@@ -12,7 +12,6 @@ def money_bag(money, value, max_coins):
 
     for k in range(1, max_coins):
         for v in range(1, value):
-            K[v, k] = 0
             for i in range(0, n):
                 if x[i] <= v and K[v - x[i], k-1] == 1:
                     K[v, k] = 1
@@ -24,7 +23,7 @@ def money_bag(money, value, max_coins):
 if __name__ == '__main__':
 
     x = [2, 5, 10, 20, 50]
-    y = 87
-    k = 4
+    y = 100
+    k = 1
 
     print(money_bag(x, y, k))
